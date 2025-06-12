d1f36e29e35b34db8edb80db1518ab2ada0d0d68
JasonX354 <xjason354@gmail.com>
2025-06-12 15:36:53 -0400

    Pop up now has realtime button that displays DIH state

--- DIH/src/App.tsx
+++ DIH/src/App.tsx
@@ -1,36 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [DIH, toggleDIH] = useState(false)
+  const [DIH_STRING, toggleDIH_String] = useState("OFF")
 
   const onclick = async () => {
-    let [tab] = await chrome.tabs.query({active: true});
-        chrome.scripting.executeScript({
-        target: {tabId: tab.id!},
-        func: () => {
-            toggleDIH(!DIH)
-            alert('Hello World');
-        }
-    });
-  }
+          toggleDIH(!DIH)
+          if (DIH) {
+            toggleDIH_String("OFF")
+          }
+          else {
+            toggleDIH_String("ON")
+          }
+        };
+  
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+
         <button onClick={onclick}>
-          DIH is currently: {DIH}
+          DIH is currently: {DIH_STRING}
         </button>
+        <p>
+          ({DIH.toString()})
+        </p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
