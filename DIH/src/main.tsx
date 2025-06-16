94490847b8fef4ce042500a771c794512fc35dc9
wwang7049 <wwang7049@gmail.com>
2025-06-16 17:05:05 -0400

    final test

--- DIH/src/main.tsx
+++ DIH/src/main.tsx
@@ -2,25 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Frame from 'react-frame-component'
-
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Frame
-      scrolling='no'
-      head={[
-        <link
-          key='0'
-          type='text/css'
-          rel='stylesheet'
-          href={chrome.runtime.getURL('/react/index.css')}
-        />,
-      ]}
-    >
-      <App />
-    </Frame>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
-
