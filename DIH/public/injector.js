88373da9c2c50d86c49bde9e6866809167c176a3
JasonX354 <xjason354@gmail.com>
2025-06-17 16:27:25 -0400

    Testing square is now draggable, but still needs to follow webpage as you scroll down

--- DIH/public/injector.js
+++ DIH/public/injector.js
@@ -4,40 +4,64 @@ chrome.storage.local.get(['DIH'], (result) => {
         //Add custom CSS 
         const Add_Custom_CSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css
 
-        function Create_Custom_Element(tag, attr_name) {
+        function Create_Custom_Element(tag, attr_name, id) {
             const custom_element = document.createElement(tag)
             custom_element.className = attr_name
+            if (id) custom_element.id = id
             document.body.append(custom_element)
         }
 
         Add_Custom_CSS(`
             .square {
-            position: fixed;
-            bottom: 5px;
-            right: 5px;
-            width: 50px;
-            height: 50px;
-            background-color: blue;
+                position: absolute;
+                bottom: 5px;
+                right: 5px;
+                width: 50px;
+                height: 50px;
+                background-color: blue;
             }
         `)
 
-        Create_Custom_Element("div", "square")
+        Create_Custom_Element("div", "square", "test")
 
-        Create_Custom_Element("div", "square")
-      }
-    });
+        // Make the DIV element draggable:
+        dragElement(document.getElementById("test"));
 
-/*
-const body = document.querySelector('body')
-const app = document.createElement('div')
+        function dragElement(elmnt) {
+            var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 
-app.id = 'react-root'
+            // Move DIV from anywhere inside the DIV:
+            elmnt.onmousedown = dragMouseDown;
 
-if(body){
-    body.prepend(app)
-}
+            function dragMouseDown(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // get the mouse cursor position at startup:
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                document.onmouseup = closeDragElement;
+                // call a function whenever the cursor moves:
+                document.onmousemove = elementDrag;
+            }
 
-const container = document.getElementById('react-root');
-const root = createRoot(container);
-root.render(<App/>)
-*/
+            function elementDrag(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // calculate the new cursor position:
+                pos1 = pos3 - e.clientX;
+                pos2 = pos4 - e.clientY;
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                // set the element's new position:
+                elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+                elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+            }
+
+            function closeDragElement() {
+                // stop moving when mouse button is released:
+                document.onmouseup = null;
+                document.onmousemove = null;
+            }
+        }
+    }
+});
