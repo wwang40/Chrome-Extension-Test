ed6f57e1098297f24442d1b456865161b106285b
JasonX354 <xjason354@gmail.com>
2025-07-04 18:08:02 -0400

    Move onDisplay to front so that it keeps its css_element property.

--- POOF/public/injector.js
+++ POOF/public/injector.js
@@ -1,8 +1,9 @@
+const onDisplay = new POOF("Testy", "test.png", 1, ["Joke", "Words of Affirmation", "Cat Picture"], "I am a POOF whose only purpose in life is to serve as a placeholder for testing.");
+
 chrome.runtime.onMessage.addListener((message) => {
     if (message.type === "POOF_UPDATE") {
         //Receive instruction to activate or deactive extension
         const POOF_ON = document.getElementById("test")
-        const onDisplay = new POOF("Testy", "test.png", 1, ["Joke", "Words of Affirmation", "Cat Picture"], "I am a POOF whose only purpose in life is to serve as a placeholder for testing.");
 
         if (message.value) { // POOF is ON
             if (!POOF_ON) { // POOF not already made
@@ -11,34 +12,28 @@ chrome.runtime.onMessage.addListener((message) => {
                     onDisplay.setUpCSS(result.positionX, result.positionY, document)
                 })
                 
-                onDisplay.Create_Custom_Element("div", "square", "test", "CLICK ME!")
+                onDisplay.Create_Custom_Element("div", "square", "test", onDisplay.getName())
 
                 onDisplay.dragElement()
 
-                onDisplay.dialogueElement()
+                onDisplay.setUpFeatures()
+
+                onDisplay.dialogueElement(document)
                 
 
                 //FRAMEWORK FOR NEXT STEP:
                 //dialogueTree(CUSTOM ELEMENT)
 
             } else { // POOF is ON and has previously been made
-                POOF_ON.style.display = 'block'
+                onDisplay.showElement()
             }
         } else { // POOF is OFF but has previously been made
             if (POOF_ON) {
-                POOF_ON.style.display = 'none'
+                onDisplay.hideElement()
             }
             return;
         }
 
-
-        //FRAMEWORK
-        //difogueTree(element, # (this will probably be a list of strings with 1 per feature))
-        //Gives the element a dialogue tree with # interactables. 
-        //element.onmousedown => spawn a # of interactables depending on element's position (mimic a dialogue bubble)
-
-        //interactable(dialogue, action)
-        //Combine with dialogueTree()?
         //when dialogue is selected/clicked, action will happen. First work on making text appear, then images, then audio, etc.
     }
 
